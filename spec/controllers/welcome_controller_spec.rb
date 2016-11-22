0cb0d2674d1d190a29e37e67c448cbee81a6d8e0
Victoria Fung <victoriahkfung@gmail.com>
2016-11-21 20:46:14 -0800

    assignment13: completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -15,4 +15,10 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 end
