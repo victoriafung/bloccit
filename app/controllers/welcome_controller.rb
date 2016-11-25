e6b06fa241391ca22bc40e57ac82812e6e428b2d
Victoria Fung <victoriahkfung@gmail.com>
2016-11-24 16:30:36 -0800

    assignment 12 completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
+
   def index
   end
 
   def about
   end
+
+  def contact
+  end
+
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+
   describe "GET index" do
     it "renders the index template" do
       get :index
