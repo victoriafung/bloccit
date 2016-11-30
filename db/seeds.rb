0b1e72d8b9507274bf4b0cf953011321410a04ac
Victoria Fung <victoriahkfung@gmail.com>
2016-11-29 23:21:21 -0800

    assignment20 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,31 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
-50.times do
-  Post.create!(
+# 50.times do
+#   Post.create!(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph
+#   )
+# end
+# posts = Post.all
+#
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: RandomData.random_paragraph
+#   )
+# end
+#
+10.times do
+  Question.create!(
     title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    body: RandomData.random_paragraph,
+    resolved: false
   )
 end
-posts = Post.all
-
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+questions = Question.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true) }
+
+  describe "attributes" do
+    it "has title, body and resolved attributes" do
+      expect(question).to have_attributes(title:"New Question Title", body: "New Question Body", resolved: true)
+    end
+  end
 end
