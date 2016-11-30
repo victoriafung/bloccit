08feb537ba03475e8ec15f3a203d3934b6d86c2c
Victoria Fung <victoriahkfung@gmail.com>
2016-11-30 00:26:17 -0800

    checkpoint20 finished topic model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :topics
   resources :posts
 
   get 'about' => 'welcome#about'
-  
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
@@ -23,5 +32,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
