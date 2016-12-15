5b9b808dfcb8893a4e93e202ccd7a65d659fa8a0
Victoria Fung <victoriahkfung@gmail.com>
2016-12-14 21:11:42 -0800

    added new_post to FavoriteMailer

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, cc: "victoriahkfung@gmail.com", subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, cc: "victoriahkfung@gmail.com", subject: "You have favorited #{post.title}")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
 
   default_scope { order('rank DESC') }
+  after_create :create_favorite
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: {minimum: 20 }, presence: true
@@ -28,5 +29,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
