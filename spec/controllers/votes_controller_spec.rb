45ba6706fbdc2936e779ca8668cb19e711beb558
Victoria Fung <victoriahkfung@gmail.com>
2016-12-15 01:15:55 -0800

    checkpoint32 completed

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 include SessionsHelper
 
 RSpec.describe VotesController, type: :controller do
-  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
-  let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:user_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: other_user) }
+  let(:my_user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:my_topic) { create(:topic) }
+  let(:user_post) { create(:post, topic: my_topic, user: my_user) }
   let(:my_vote) { Vote.create!(value: 1) }
 
   context "guest" do
@@ -103,7 +103,7 @@ RSpec.describe VotesController, type: :controller do
       it ":back redirects to posts topic show" do
         request.env["HTTP_REFERER"] = topic_path(my_topic)
         post :down_vote, post_id: user_post.id
-        
+
         expect(response).to redirect_to(my_topic)
       end
     end
